16563d17996822648bf90ac24f5ca0b89be0d309
saleh <saleh@alghusson.com>
2019-12-25 00:02:28 -0500

    Updates

---
+++ precache-manifest.a92b58a62b8b2131a7309510d45cabc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d141091734882957c89f148dfd93d534",
+    "url": "/nvideohighlights/index.html"
+  },
+  {
+    "revision": "2a15d9802d97f0abb977",
+    "url": "/nvideohighlights/static/css/2.4058c193.chunk.css"
+  },
+  {
+    "revision": "051f5e43b6d1a5496187",
+    "url": "/nvideohighlights/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a15d9802d97f0abb977",
+    "url": "/nvideohighlights/static/js/2.ed6d0f3c.chunk.js"
+  },
+  {
+    "revision": "30d8ef3813de0269a8460a5317dd0eba",
+    "url": "/nvideohighlights/static/js/2.ed6d0f3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "051f5e43b6d1a5496187",
+    "url": "/nvideohighlights/static/js/main.d25062da.chunk.js"
+  },
+  {
+    "revision": "5afbe299b95aeb1885a8",
+    "url": "/nvideohighlights/static/js/runtime-main.559870b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.c3bcf2b0047d444d8e76be9438753208.js"
+  "/nvideohighlights/precache-manifest.a92b58a62b8b2131a7309510d45cabc3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nvideohighlights/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
